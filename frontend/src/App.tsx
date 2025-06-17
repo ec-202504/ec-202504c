0aa0a1a83dfe380f73839811956935524ac75c98
Kazuma <kazu15870@gmail.com>
2025-06-17 21:24:51 +0900

    chore: shadcnを導入

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return (
-    <>
-      <h1>TOP Page</h1>
-    </>
-  );
+	return (
+		<>
+			<div className="flex min-h-svh flex-col items-center justify-center">
+				<Button>Click me</Button>
+			</div>
+		</>
+	);
 }
 
 export default App;
