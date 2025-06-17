c14bf76e9fcb120b72bbe190eddade08f0004073
Kazuma <kazu15870@gmail.com>
2025-06-17 22:44:10 +0900

    chore: ディレクトリ構成を修正

---
+++ frontend/src/routes/about/index.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router";
+import AboutPage from "../../features/about/AboutPage";
+
+export const Route = createFileRoute("/about/")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return <AboutPage />;
+}
