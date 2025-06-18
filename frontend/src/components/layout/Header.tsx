b15ab1dee3542c896ca8f7b7257334364059423c
Kazuuma-19 <kazu15870@gmail.com>
2025-06-18 17:10:38 +0900

    feat: headerを追加

---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,49 @@
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
+import { useState } from "react";
+import { Link } from "@tanstack/react-router";
+
+function Header() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  return (
+    <header className="w-full px-4 py-2 flex items-center bg-white shadow">
+      <div className="text-xl font-semibold">
+        <Link to="/product">ECサイト</Link>
+      </div>
+
+      <Separator orientation="vertical" className="h-6 mx-4" />
+
+      <nav className="flex items-center gap-3 ml-auto">
+        <Link to="/product/recommend" className="hover:underline">
+          レコメンド
+        </Link>
+        <Link to="/order/history" className="hover:underline">
+          注文履歴
+        </Link>
+        <Link to="/cart" className="hover:underline">
+          カート
+        </Link>
+
+        <Separator orientation="vertical" className="h-6 mx-2" />
+
+        {isLogin ? (
+          <Button variant="outline">ログアウト</Button>
+        ) : (
+          <>
+            <Link to="/login">
+              <Button variant="outline" className="mr-0.5">
+                ログイン
+              </Button>
+            </Link>
+            <Link to="/register">
+              <Button variant="default">新規登録</Button>
+            </Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
