e78bb2ee365a80a275fb37ef9e5afc799822b312
Kazuuma-19 <kazu15870@gmail.com>
2025-06-19 12:12:58 +0900

    chore: sqlを修正

--- backend/document/sql/insert-oss.sql
+++ backend/document/sql/insert-oss.sql
@@ -1,5 +1,5 @@
-INSERT INTO OSs (id, name) VALUES (1, 'Windows 11');
-INSERT INTO OSs (id, name) VALUES (2, 'macOS Sonoma');
-INSERT INTO OSs (id, name) VALUES (3, 'Ubuntu 24.04');
-INSERT INTO OSs (id, name) VALUES (4, 'Fedora 40');
-INSERT INTO OSs (id, name) VALUES (5, 'Chrome OS');
+INSERT INTO OSs (os_id, name) VALUES (1, 'Windows 11');
+INSERT INTO OSs (os_id, name) VALUES (2, 'macOS Sonoma');
+INSERT INTO OSs (os_id, name) VALUES (3, 'Ubuntu 24.04');
+INSERT INTO OSs (os_id, name) VALUES (4, 'Fedora 40');
+INSERT INTO OSs (os_id, name) VALUES (5, 'Chrome OS');
--- backend/document/sql/insert-purposeforpc.sql
+++ backend/document/sql/insert-purposeforpc.sql
@@ -1,5 +1,5 @@
-INSERT INTO Purposes (id, name) VALUES (1, 'Gaming');
-INSERT INTO Purposes (id, name) VALUES (2, 'Office');
-INSERT INTO Purposes (id, name) VALUES (3, 'Development');
-INSERT INTO Purposes (id, name) VALUES (4, 'Design');
-INSERT INTO Purposes (id, name) VALUES (5, 'Education');
+INSERT INTO Purposes (purpose_id, name) VALUES (1, 'Gaming');
+INSERT INTO Purposes (purpose_id, name) VALUES (2, 'Office');
+INSERT INTO Purposes (purpose_id, name) VALUES (3, 'Development');
+INSERT INTO Purposes (purpose_id, name) VALUES (4, 'Design');
+INSERT INTO Purposes (purpose_id, name) VALUES (5, 'Education');
