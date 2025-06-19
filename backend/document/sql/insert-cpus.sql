e6b7d290bb4e3229abc944da2410bccfffcf7b5c
Kazuuma-19 <kazu15870@gmail.com>
2025-06-19 12:12:58 +0900

    chore: sqlを修正

--- backend/document/sql/insert-cpus.sql
+++ backend/document/sql/insert-cpus.sql
@@ -1,5 +1,5 @@
-INSERT INTO CPUs (id, name) VALUES (1, 'Intel Core i5');
-INSERT INTO CPUs (id, name) VALUES (2, 'Intel Core i7');
-INSERT INTO CPUs (id, name) VALUES (3, 'AMD Ryzen 5');
-INSERT INTO CPUs (id, name) VALUES (4, 'AMD Ryzen 7');
-INSERT INTO CPUs (id, name) VALUES (5, 'Apple M2');
+INSERT INTO CPUs (cpu_id, name) VALUES (1, 'Intel Core i5');
+INSERT INTO CPUs (cpu_id, name) VALUES (2, 'Intel Core i7');
+INSERT INTO CPUs (cpu_id, name) VALUES (3, 'AMD Ryzen 5');
+INSERT INTO CPUs (cpu_id, name) VALUES (4, 'AMD Ryzen 7');
+INSERT INTO CPUs (cpu_id, name) VALUES (5, 'Apple M2');
