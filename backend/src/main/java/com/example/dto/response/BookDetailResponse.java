5c3ab1646b0133d38a0b385d447c25abc2feff51
Kazuuma-19 <kazu15870@gmail.com>
2025-06-20 17:46:00 +0900

    fix: bookの型をフロントと統一

--- backend/src/main/java/com/example/dto/response/BookDetailResponse.java
+++ backend/src/main/java/com/example/dto/response/BookDetailResponse.java
@@ -1,8 +1,15 @@
 package com.example.dto.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /** 書籍の詳細情報レスポンスを表すDTOクラス. */
+@Getter
+@Setter
+@NoArgsConstructor
 public class BookDetailResponse {
-  private Integer productId;
+  private Integer bookId;
   private String name;
   private String imageUrl;
   private String author;
