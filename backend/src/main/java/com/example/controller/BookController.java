fef8619bc5d2f1f2fe5c62f95bcc14fac5fba06f
Kazuma Saito <96773919+Kazuuma-19@users.noreply.github.com>
2025-06-20 10:07:27 +0900

    Update BookController.java

--- backend/src/main/java/com/example/controller/BookController.java
+++ backend/src/main/java/com/example/controller/BookController.java
@@ -1,10 +1,10 @@
 package com.example.controller;
 
 import com.example.dto.request.AddBookRequest;
+import com.example.dto.request.UpdateBookRequest;
 import com.example.model.Book;
 import com.example.model.Language;
 import com.example.model.Purpose;
-import com.example.dto.request.UpdateBookRequest;
 import com.example.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
