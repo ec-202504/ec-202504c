b56ec2afa6a0814b942bd7092b8de523061425aa
CrovArkhangel <SaToShi.KR.1123@gmail.com>
2025-06-23 17:09:18 +0900

    未使用のメソッドの削除

--- frontend/src/features/product/utils/searchHandlers.ts
+++ frontend/src/features/product/utils/searchHandlers.ts
@@ -1,5 +1,3 @@
-import type { SearchParams, PcFilters, BookFilters } from "../types/filters";
-
 /**
  * 検索フォームの送信ハンドラー
  */
@@ -10,34 +8,4 @@ export const createSearchSubmitHandler = (
     e.preventDefault();
     handleSearch(query);
   };
-};
-
-/**
- * URLパラメータから検索条件を構築
- */
-export const buildSearchParams = (
-  selectedTab: string,
-  pcFilters: PcFilters,
-  bookFilters: BookFilters,
-  query: string,
-  page: number,
-): SearchParams => {
-  const params: SearchParams = {
-    tab: selectedTab,
-    page: page.toString(),
-    query,
-  };
-
-  if (selectedTab === "pcs") {
-    if (pcFilters.osId) params.osId = pcFilters.osId;
-    if (pcFilters.cpuId) params.cpuId = pcFilters.cpuId;
-    if (pcFilters.gpuId) params.gpuId = pcFilters.gpuId;
-    if (pcFilters.purposeId) params.purposeId = pcFilters.purposeId;
-    if (pcFilters.deviceType) params.deviceType = pcFilters.deviceType;
-  } else {
-    if (bookFilters.languageId) params.languageId = bookFilters.languageId;
-    if (bookFilters.purposeId) params.purposeId = bookFilters.purposeId;
-  }
-
-  return params;
 };
