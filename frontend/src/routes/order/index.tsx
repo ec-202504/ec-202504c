342b63dc2883464e32e47b1693bda29fc8738cb0
daiki9898 <dddaiki3150@gmail.com>
2025-06-24 17:18:19 +0900

    注文確認画面をログイン前に表示できないようにした

--- frontend/src/routes/order/index.tsx
+++ frontend/src/routes/order/index.tsx
@@ -1,7 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import OrderPage from "../../features/order/OrderPage";
+import { userAtom } from "../../stores/compareAtom";
+import { getDefaultStore } from "jotai";
 
 export const Route = createFileRoute("/order/")({
+  beforeLoad: async () => {
+    // Fix: useAtomValueを使えないため、デフォルトストアから取得しているが、非推奨
+    const store = getDefaultStore();
+    const user = store.get(userAtom);
+    // ログインしていない場合はログインページにリダイレクト
+    if (!user) {
+      throw redirect({ to: "/user/login" });
+    }
+  },
   component: RouteComponent,
 });
 
